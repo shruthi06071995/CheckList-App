31d6aa6196e61e0cb94be5fa313765797c068bf2
shruthi06071995 <shruthiganesan518@gmail.com>
2026-02-10 20:05:21 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css'
-import logo from "./assets/tiruchendur.png"
+import logo from "./assets/tiruchendurlogo.png"
 
 function App() {
   const [input, setInput] = useState("");
@@ -49,64 +49,66 @@ function App() {
 
   return (
     <>
-      <div className="container" style={{}}>
-                <img style={{ width: "200px" }} src={logo} alt="logo" />
-                <div className="container mt-5">
-                    <h3 className="text-center">
-                        Tiruchendur Trip – Packing Todo List
-                    </h3>
+      <div className="container head" >
+        <div className='container' style={{padding: "60px 650px"}}>
+          <img style={{ width: "200px" }} src={logo} alt="logo" />
+        </div>
+        <div className="container mt-5" style={{padding: "60px", textAlign: "center"}} >
+          <h3 className="text-center" style={{fontSize: "30px",fontStyle: "unset" }}>
+            <b><u>Tiruchendur Trip – Packing Todo List</u></b>
+          </h3>
 
-                    {/* Input */}
-                    <div className="input-group mb-3">
-                        <input
-                            className="form-control"
-                            placeholder="Add item to pack"
-                            value={input}
-                            onChange={(e) => setInput(e.target.value)}
-                        />
-                        <button className="btn btn-success" onClick={addItem}>
-                            Add
-                        </button>
-                    </div>
+          {/* Input */}
+          <div className="input-group mb-3">
+            <input style={{padding: "9px 55px", borderRadius: "5px" }}
+              className="form-control"
+              placeholder="Add item to pack"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button style={{padding: "10px 30px", borderRadius: "5px",}} className="btn btn-success" onClick={addItem}>
+              Add
+            </button>
+          </div>
 
-                    {/* Todo List */}
-                    <ul className="list-group">
-                        {list.map((item) => (
-                            <li
-                                key={item.id}
-                                className="list-group-item d-flex justify-content-between align-items-center"
-                            >
-                                <div>
-                                    <input
-                                        type="checkbox"
-                                        className="form-check-input me-2"
-                                        checked={item.completed}
-                                        onChange={() => toggleItem(item.id)}
-                                    />
+          {/* Todo List */}
+          <ul className="list-group">
+            {list.map((item) => (
+              <li
+                key={item.id}
+                className="list-group-item d-flex justify-content-between align-items-center"
+              >
+                <div>
+                  <input
+                    type="checkbox"
+                    className="form-check-input me-2"
+                    checked={item.completed}
+                    onChange={() => toggleItem(item.id)}
+                  />
 
-                                    <span
-                                        style={{
-                                            textDecoration: item.completed
-                                                ? "line-through"
-                                                : "none"
-                                        }}
-                                    >
-                                        {item.text}
-                                    </span>
-                                </div>
-
-                                <button
-                                    className="btn btn-outline-danger btn-sm"
-                                    onClick={() => removeItem(item.id)}
-                                >
-                                    Remove
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
+                  <span
+                    style={{
+                      textDecoration: item.completed
+                        ? "line-through"
+                        : "none"
+                    }}
+                  >
+                    {item.text}
+                  </span>
                 </div>
-            </div>
-        </>
+
+                <button
+                  className="btn btn-outline-danger btn-sm"
+                  onClick={() => removeItem(item.id)}
+                >
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
 
